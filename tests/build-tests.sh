d348067cc32991d8747f2a428b3b8a75361172f4
Nikola Markovic <nikola.m@gmail.com>
2022-10-18 13:28:28 -0500

    Change cJSON URL from git to HTTPS to avoid permission issues

--- tests/build-tests.sh
+++ tests/build-tests.sh
@@ -5,7 +5,7 @@ this_dir=$(dirname "$0")
 pushd "$this_dir"
 set -e
 if [[ ! -d cJSON ]]; then
-  git clone --depth 1 --branch v1.7.13 git://github.com/DaveGamble/cJSON.git
+  git clone --depth 1 --branch v1.7.13 https://github.com/DaveGamble/cJSON.git
 fi
 test -d cJSON
 
