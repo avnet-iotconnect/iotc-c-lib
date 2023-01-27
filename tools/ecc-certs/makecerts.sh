3b86489e7d3534cd77b522a2a4bb15d0279d189f
nbeck@witekio.com <nbeck@witekio.com>
2023-01-27 13:19:21 +0000

    Support creation of EC and RSA key types for use with device certificates. Also generate DER format keys and certs (both as binary files and C array formatted)

--- tools/ecc-certs/makecerts.sh
+++ tools/ecc-certs/makecerts.sh
@@ -15,7 +15,9 @@ $0 <command> ...
  command:
   root - Creates a root certificate per config file
   verify <code> - Creates a a verification certificate with supplied code as Common Name
-  deice <name> - Creates an ECC device certificate and private key with specified name
+  device_ecc <name> - Creates an ECC device certificate and private key with specified name
+  device_rsa1024 <name> - Creates an ECC device certificate and private key with specified name
+  device_rsa2048 <name> - Creates an ECC device certificate and private key with specified name
 END
 
   exit 250
@@ -58,12 +60,37 @@ function verify {
   rm -f "certs/${code}-verify-key.pem" "certs/${code}-verify.csr"
 }
 
+function device_ecc {
+  device ecc $@
+}
+
+function device_rsa1024 {
+  device rsa1024 $@
+}
+
+function device_rsa2048 {
+  device rsa2048 $@
+}
+
 function device {
-  local name=$1
+  local key_type=$1
+  local name=$2
+  local key_conversion_params=""
   if [[ -z "${name}" ]]; then
      usageAndExit "Device name argument is required"
   fi
-  openssl ecparam -name secp256k1 -genkey -noout -out "${CPID}/${name}-key.pem"
+  if [ "${key_type}" == "ecc" ]; then
+    openssl ecparam -name secp256k1 -genkey -noout -out "${CPID}/${name}-key.pem"
+    key_conversion_params="ec"
+  elif [ "${key_type}" == "rsa1024" ]; then
+    openssl genrsa -out "${CPID}/${name}-key.pem" 1024
+    key_conversion_params="rsa"
+  elif [ "${key_type}" == "rsa2048" ]; then
+    openssl genrsa -out "${CPID}/${name}-key.pem" 2048
+    key_conversion_params="rsa"
+  else
+    usageAndExit "Invalid crypto type. Supported key types are `ecc`, `rsa1024` (which creates 1024bit key) & `rsa2048` (which creates 2048bit key)"
+  fi
   openssl req -new -key "${CPID}/${name}-key.pem" -out "${CPID}/${name}.csr" \
     -subj "$(genSubject "${CPID}-${name}")"
 
@@ -75,11 +102,18 @@ function device {
     -out "${CPID}/${name}-crt.pem" \
     -days ${X509_VALIDITY}
 
+  openssl ${key_conversion_params} -outform der -in "${CPID}/${name}-key.pem" -out "${CPID}/${name}-key.der"
+  openssl x509 -outform der -in "${CPID}/${name}-crt.pem" -out "${CPID}/${name}-crt.der"
+
   f="${CPID}/${name}-c-snippets.txt"
   echo > $f
   pem2c "${CPID}/${name}-key.pem" >> $f
   pem2c "${CPID}/${name}-crt.pem" >> $f
   rm -f "${CPID}/${name}.csr"
+
+  f="${CPID}/${name}-c-array.txt"
+  xxd -i ${CPID}/${name}-crt.der > ${f}
+  xxd -i ${CPID}/${name}-key.der >> ${f}
 }
 
 mkdir -p certs
