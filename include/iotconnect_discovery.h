ec87e0eeea66a18b42ef7d33f229f579627c73ec
Neil Matthews <nmatthews@witekio.com>
2023-09-07 17:12:38 +0100

    Move discovery host, etc. details to iotc-generic-c-sdk

--- include/iotconnect_discovery.h
+++ include/iotconnect_discovery.h
@@ -18,40 +18,6 @@ extern "C" {
 
 #include "iotconnect_lib_config.h"
 
-#ifndef IOTCONNECT_DISCOVERY_HOSTNAME
-#define IOTCONNECT_DISCOVERY_HOSTNAME "discovery.iotconnect.io"
-#endif
-
-///////////////////////////////////////////////////////////////////////////////////
-// you need to pass cpid , env and the HOST at GET_TEMPLATE
-// This templates can be used for raw HTTP headers in case that the platform doesn't GET/POST functionality
-#define IOTCONNECT_DISCOVERY_HEADER_TEMPLATE \
-    "GET /api/sdk/cpid/%s/lang/M_C/ver/2.0/env/%s HTTP/1.1\r\n" \
-    "Host: " IOTCONNECT_DISCOVERY_HOSTNAME "\r\n" \
-    "Connection: close\r\n" \
-    "\r\n"
-
-///////////////////////////////////////////////////////////////////////////////
-// This templates can be used for raw HTTP headers in case that the platform doesn't GET/POST functionality
-// you need to pass URL returned from discovery host ,host form discovery host, post_data_lan and post_data
-#define IOTCONNECT_SYNC_HEADER_TEMPLATE \
-    "POST %s/sync? HTTP/1.1\r\n" \
-    "Host: %s\r\n" \
-    "Content-Type: application/json; charset=utf-8\r\n" \
-    "Connection: close\r\n" \
-    "Content-length: %d\r\n" \
-    "\r\n" \
-    "%s"
-
-// You will typically use this JSON post data to get mqtt client information
-#define IOTCONNECT_DISCOVERY_PROTOCOL_POST_DATA_TEMPLATE "{\"cpId\":\"%s\",\"uniqueId\":\"%s\",\"option\":{\"attribute\":false,\"setting\":false,\"protocol\":true,\"device\":false,\"sdkConfig\":false,\"rule\":false}}"
-
-// add 1 for string terminator
-#define IOTCONNECT_DISCOVERY_PROTOCOL_POST_DATA_MAX_LEN (\
-    sizeof(IOTCONNECT_DISCOVERY_PROTOCOL_POST_DATA_TEMPLATE) + \
-    CONFIG_IOTCONNECT_DUID_MAX_LEN + CONFIG_IOTCONNECT_CPID_MAX_LEN \
-    )
-
 typedef enum {
     IOTCL_SR_OK = 0,
     IOTCL_SR_DEVICE_NOT_REGISTERED = 1,
@@ -101,7 +67,6 @@ void iotcl_discovery_free_discovery_response(IotclDiscoveryResponse *response);
 IotclSyncResponse *iotcl_discovery_parse_sync_response(const char *response_data);
 
 void iotcl_discovery_free_sync_response(IotclSyncResponse *response);
-
 
 #ifdef __cplusplus
 }
